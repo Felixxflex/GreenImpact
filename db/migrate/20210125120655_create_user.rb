bc4ab0547730b2d6ae158c7c6ece74f347fb5bd3
Felix Hermann <17felix2000@gmail.com>
2021-01-25 13:08:21 +0100

    new user db

--- db/migrate/20210125120655_create_user.rb
+++ db/migrate/20210125120655_create_user.rb
@@ -1,4 +1,4 @@
-class CreateUsertable < ActiveRecord::Migration[6.0]
+class CreateUser < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       t.string :email, null: false, default: ""
